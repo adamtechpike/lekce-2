9c12d0a661c446583720b4acf05b40b93bb441ab
adamtechpike <>
2018-09-28 13:16:38 +0000

    úkol s trojuhelnikem a obdelnikem

--- pythagoras.php
+++ pythagoras.php
@@ -3,4 +3,14 @@ $a = 5;
 $b = 4;
 $obdelnikobsah = $a*$b;
 echo 'Obsah obdelníku o stranách ' . $a . 'cm a ' . $b . 'cm je ' . $obdelnikobsah . 'cm2';
+?>
+<br></br>
+<?php
+$strana = 6;
+$uhel = 60;
+$vyska = sin(deg2rad($uhel))*$strana;
+$trojuhelnikobsah = ($strana*$vyska)/2; 
+echo 'Rovnostranný trojúhelník o straně délky ' . $strana . 'cm a úhlu ' . $uhel . ' stupňů je ' . round($trojuhelnikobsah) . 'cm2';
+?>
+
 
