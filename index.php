59c91973b237e4721efcc982675ac90376be908d
adamtechpike <>
2018-09-28 09:08:09 +0000

    příprava zadání úkolu - obdelník

--- index.php
+++ index.php
@@ -59,9 +59,6 @@
   echo 'Centrální obrazovka všehomíra';
 
 ?>
-        </h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
 
     </main><!-- /.container -->
 
---
+++ pythagoras.php
@@ -0,0 +1,6 @@
+<?php
+$a = 5;
+$b = 4;
+$obdelnikobsah = $a*$b;
+echo 'Obsah obdelníku o stranách ' . $a . 'cm a ' . $b . 'cm je ' . $obdelnikobsah . 'cm2';
+
